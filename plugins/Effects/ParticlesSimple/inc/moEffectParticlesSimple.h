3ef14cddef06b30548837b133b1191f6909c81d0
fabricio_costa <fabricio_costa@c963c68c-f84c-0410-a3d6-bef16c67260e>
2009-04-18 16:30:27 +0000

    Particle Simple Plugin
    git-svn-id: https://moldeo.svn.sourceforge.net/svnroot/moldeo/trunk/0.7@145 c963c68c-f84c-0410-a3d6-bef16c67260e

---
+++ plugins/Effects/ParticlesSimple/inc/moEffectParticlesSimple.h
@@ -0,0 +1,415 @@
+/*******************************************************************************
+
+                              moEffectParticlesSimple.h
+
+  ****************************************************************************
+  *                                                                          *
+  *   This source is free software; you can redistribute it and/or modify    *
+  *   it under the terms of the GNU General Public License as published by   *
+  *   the Free Software Foundation; either version 2 of the License, or      *
+  *  (at your option) any later version.                                    *
+  *                                                                          *
+  *   This code is distributed in the hope that it will be useful, but       *
+  *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
+  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+  *   General Public License for more details.                               *
+  *                                                                          *
+  *   A copy of the GNU General Public License is available on the World     *
+  *   Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also       *
+  *   obtain it by writing to the Free Software Foundation,                  *
+  *   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.         *
+  *                                                                          *
+  ****************************************************************************
+
+  Copyright(C) 2006 Fabricio Costa
+
+  Authors:
+  Fabricio Costa
+
+
+  todo:
+  1) ColorIn
+  2) ColorOut
+  3) BreakIn
+  4) BreakOut
+
+
+
+  5) Script general
+  6) Script por particula ( se ejecuta por cada particula ) ( acceso a eventos [tecla presionada...] )
+
+  *) movimiento y posicionamieno de particuclas....
+
+  7) parametro desfazaje de movimiento??? que sea una opcion...
+
+*******************************************************************************/
+
+#ifndef __MO_EFFECT_PARTICLES_H__
+#define __MO_EFFECT_PARTICLES_H__
+
+#include "moPlugin.h"
+#include "moScript.h"
+
+#define MO_PARTICLES_TRANSLATE_X 0
+#define MO_PARTICLES_TRANSLATE_Y 1
+#define MO_PARTICLES_SCALE_X 2
+#define MO_PARTICLES_SCALE_Y 3
+
+enum moParticlesSimpleEmitterType {
+  PARTICLES_EMITTERTYPE_GRID = 0,
+  PARTICLES_EMITTERTYPE_SPHERE = 1,
+  PARTICLES_EMITTERTYPE_TUBE = 2,
+  PARTICLES_EMITTERTYPE_JET = 3,
+  PARTICLES_EMITTERTYPE_POINT = 4,
+  PARTICLES_EMITTERTYPE_TRACKER = 5
+};
+
+enum moParticlesSimpleAttractorType {
+  PARTICLES_ATTRACTORTYPE_POINT = 0,
+  PARTICLES_ATTRACTORTYPE_VERTEX = 1,
+  PARTICLES_ATTRACTORTYPE_VORTEX = 2,
+  PARTICLES_ATTRACTORTYPE_DISC = 3,
+  PARTICLES_ATTRACTORTYPE_FLOOR = 4,
+  PARTICLES_ATTRACTORTYPE_STRANGE = 5
+};
+
+enum moParticlesSimpleTextureMode {
+    PARTICLES_TEXTUREMODE_UNIT = 0,
+    PARTICLES_TEXTUREMODE_PATCH = 1
+};
+
+enum moParticlesCreationMethod {
+    PARTICLES_CREATIONMETHOD_LINEAR=0, ///particles are been created along the array order
+    PARTICLES_CREATIONMETHOD_PLANAR=1, ///particles are been created randomly ober the surface of the emitter geometry
+    PARTICLES_CREATIONMETHOD_VOLUMETRIC=2 ///particles are been created randomly inside the volume defined by the emitter geometry
+};
+
+enum moParticlesRandomMethod {
+    PARTICLES_RANDOMMETHOD_NOISY=0,
+    PARTICLES_RANDOMMETHOD_COLINEAR=1,
+    PARTICLES_RANDOMMETHOD_PERPENDICULAR=2
+};
+
+enum moParticlesOrientationMode {
+    PARTICLES_ORIENTATIONMODE_FIXED=0,
+    PARTICLES_ORIENTATIONMODE_CAMERA=1,
+    PARTICLES_ORIENTATIONMODE_MOTION=2
+};
+
+enum moParticlesSimpleParamIndex {
+	PARTICLES_ALPHA,
+	PARTICLES_COLOR,
+	PARTICLES_PARTICLECOLOR,
+	PARTICLES_SYNC,
+	PARTICLES_PHASE,
+	PARTICLES_FONT,
+	PARTICLES_TEXT,
+	PARTICLES_TEXTURE,
+	PARTICLES_TEXTUREMODE,
+	PARTICLES_BLENDING,
+	PARTICLES_TIMETOREVELATION,
+	PARTICLES_TIMEOFREVELATION,
+	PARTICLES_TIMETORESTORATION,
+	PARTICLES_TIMEOFRESTORATION,
+	PARTICLES_DRAWINGFEATURES,
+
+	PARTICLES_WIDTH,
+	PARTICLES_HEIGHT,
+	PARTICLES_SIZEX,
+	PARTICLES_SIZEY,
+	PARTICLES_SIZEZ,
+
+	PARTICLES_GRAVITY,
+	PARTICLES_VISCOSITY,
+
+	PARTICLES_MAXAGE,
+	PARTICLES_RANDOMMETHOD,
+	PARTICLES_CREATIONMETHOD,
+	PARTICLES_ORIENTATIONMODE,
+	PARTICLES_EMITIONPERIOD,
+	PARTICLES_EMITIONRATE,
+	PARTICLES_DEATHPERIOD,
+	PARTICLES_SCRIPT,
+
+	PARTICLES_FADEIN,
+	PARTICLES_FADEOUT,
+	PARTICLES_SIZEIN,
+	PARTICLES_SIZEOUT,
+
+	PARTICLES_RANDOMPOSITION,
+	PARTICLES_RANDOMPOSITION_X,
+	PARTICLES_RANDOMPOSITION_Y,
+	PARTICLES_RANDOMPOSITION_Z,
+
+	PARTICLES_RANDOMVELOCITY,
+	PARTICLES_RANDOMVELOCITY_X,
+	PARTICLES_RANDOMVELOCITY_Y,
+	PARTICLES_RANDOMVELOCITY_Z,
+
+	PARTICLES_RANDOMMOTION,
+	PARTICLES_RANDOMMOTION_X,
+	PARTICLES_RANDOMMOTION_Y,
+	PARTICLES_RANDOMMOTION_Z,
+
+
+	PARTICLES_EMITTERTYPE,
+	PARTICLES_EMITTERVECTOR_X,
+	PARTICLES_EMITTERVECTOR_Y,
+	PARTICLES_EMITTERVECTOR_Z,
+
+	PARTICLES_ATTRACTORTYPE,
+	PARTICLES_ATTRACTORVECTOR_X,
+	PARTICLES_ATTRACTORVECTOR_Y,
+	PARTICLES_ATTRACTORVECTOR_Z,
+
+    PARTICLES_SCALEX_PARTICLE,
+	PARTICLES_SCALEY_PARTICLE,
+	PARTICLES_SCALEZ_PARTICLE,
+	PARTICLES_ROTATEX_PARTICLE,
+	PARTICLES_ROTATEY_PARTICLE,
+	PARTICLES_ROTATEZ_PARTICLE,
+	PARTICLES_TRANSLATEX,
+	PARTICLES_TRANSLATEY,
+	PARTICLES_TRANSLATEZ,
+    PARTICLES_SCALEX,
+	PARTICLES_SCALEY,
+	PARTICLES_SCALEZ,
+	PARTICLES_ROTATEX,
+	PARTICLES_ROTATEY,
+	PARTICLES_ROTATEZ,
+	PARTICLES_EYEX,
+	PARTICLES_EYEY,
+	PARTICLES_EYEZ,
+	PARTICLES_VIEWX,
+	PARTICLES_VIEWY,
+	PARTICLES_VIEWZ,
+	PARTICLES_LIGHTX,
+	PARTICLES_LIGHTY,
+	PARTICLES_LIGHTZ,
+    PARTICLES_INLET,
+	PARTICLES_OUTLET
+};
+
+class moParticlesSimplePhysics : public moAbstract {
+
+  public:
+
+    moVector3f      m_GravityCenter;//x,y,z,intensity
+    moVector3f      m_GravityVector;//x,y,z,intensity
+    moVector3f      m_GravityDisc;//normal x, normal y, normal z,intensity
+
+    moVector3f      m_Plane;
+
+    long            m_MaxAge;
+    long            m_EmitionPeriod;
+    long            m_EmitionRate;
+    long            m_DeathPeriod;
+
+    moParticlesCreationMethod   m_CreationMethod;
+    moParticlesRandomMethod     m_RandomMethod;
+    moParticlesOrientationMode  m_OrientationMode;
+
+
+    double          m_FadeIn;
+    double            m_FadeOut;
+    double            m_SizeIn;
+    double            m_SizeOut;
+
+    moParticlesSimpleEmitterType    m_EmitterType;
+    moVector3f      m_EmitterSize;
+
+    moParticlesSimpleAttractorType    m_AttractorType;
+
+    moVector3f      m_EyeVector;
+
+    moVector3f      m_EmitterVector;
+    moVector3f      m_AttractorVector;
+
+    double          m_RandomVelocity;
+    moVector3f      m_VelocityVector;
+
+    double           m_RandomPosition;
+    moVector3f      m_PositionVector;
+
+    double          m_RandomMotion;
+    moVector3f      m_MotionVector;
+
+    double gravitational;
+    double viscousdrag;
+
+    moTimer EmitionTimer;
+
+    moText  m_ParticleScript;
+
+
+};
+
+class moParticlesSimple : public moAbstract {
+
+  public:
+
+    moVector3f  Pos3d;
+    moVector3f  Velocity;
+    moVector3f  Force;
+
+    moVector2f  Pos;//relativo a una grilla
+    moVector2f  TCoord;
+    moVector2f  Size;
+    moVector2f  TSize;
+
+    moVector3f  dpdt;
+    moVector3f  dvdt;
+
+    double      Mass;
+    bool        Fixed;
+    bool        Visible;
+
+    double      Alpha;
+    double      Scale;
+
+    MOint       GLId;///para asociar la textura al momento de la creación
+
+    moTimer     Age;
+};
+
+
+enum enumRevelationStatus {
+
+    PARTICLES_FULLRESTORED = 0,
+    PARTICLES_REVEALING = 1,
+    PARTICLES_REVEALINGALL = 2,
+    PARTICLES_FULLREVEALED = 3,
+    PARTICLES_RESTORING = 4,
+    PARTICLES_RESTORINGALL = 5
+};
+
+
+
+moDeclareDynamicArray( moParticlesSimple*, moParticlesSimpleArray )
+
+class moEffectParticlesSimple : public moEffect
+{
+    public:
+
+
+        moEffectParticlesSimple();
+        virtual ~moEffectParticlesSimple();
+
+        MOboolean Init();
+        void Draw(moTempo*, moEffectState* parentstate = NULL);
+        MOboolean Finish();
+
+        void Interaction( moIODeviceManager * );
+        moConfigDefinition * GetDefinition( moConfigDefinition *p_configdefinition );
+
+        int ScriptCalling(moLuaVirtualMachine& vm, int iFunctionNumber);
+        void HandleReturns(moLuaVirtualMachine& vm, const char *strFunc);
+
+
+    private:
+
+        void UpdateParameters();
+
+        void ResetTimers();
+        void RevealingAll();
+        void RestoringAll();
+
+
+        void InitParticlesSimple( int p_cols, int p_rows );
+
+        void CalculateForces( bool tmparray = false );
+        void CalculateDerivatives( bool tmparray = false, double dt = 0.0 );
+
+        void UpdateParticles( double dt, int method );
+        void Regenerate();
+        double Solver1D( double h, double y0, int method, double (*fcn)(double) );
+
+        void SetParticlePosition( moParticlesSimple* pParticle );
+
+
+        void ParticlesSimpleInfluence( float posx, float posy, float velx, float vely, float veln );
+        void ParticlesSimpleAnimation( moTempo* tempogral, moEffectState* parentstate );
+        void DrawParticlesSimple( moTempo* tempogral, moEffectState* parentstate );
+        void DrawTracker();
+
+        /// Script functions.
+        void RegisterFunctions();
+
+        int LGetTicks(moLuaVirtualMachine& vm);
+        int LSetTicks(moLuaVirtualMachine& vm);
+
+        int LGetPreconf(moLuaVirtualMachine& vm);
+        int LSetPreconf(moLuaVirtualMachine& vm);
+
+        int LGetParticle(moLuaVirtualMachine& vm);
+        int LGetParticlePosition(moLuaVirtualMachine& vm);
+        int LGetParticleVelocity(moLuaVirtualMachine& vm);
+
+        int LUpdateParticle( moLuaVirtualMachine& vm );
+        int LUpdateParticlePosition( moLuaVirtualMachine& vm );
+        int LUpdateParticleVelocity( moLuaVirtualMachine& vm );
+
+        int LUpdateForce( moLuaVirtualMachine& vm );
+
+        int PushDebugString(moLuaVirtualMachine& vm);
+
+
+        ///end script functions
+
+
+        moParticlesSimpleArray    m_ParticlesSimpleArray;
+        moParticlesSimpleArray    m_ParticlesSimpleArrayTmp;
+        moParticlesSimplePhysics    m_Physics;
+
+        bool                    m_bTrackerInit;
+        moTrackerSystemData*    m_pTrackerData;
+        moVector2f              m_TrackerBarycenter;
+
+
+        void setUpLighting();
+
+        int m_rows,m_cols;
+        float normalf; ///width of full floor usually 100.0f
+
+        long time_tofull_revelation;
+        long time_tofull_restoration;
+        long time_of_revelation;
+        long time_of_restoration;
+        long drawing_features; /// 0: nothing 1: motion  2: all
+        long texture_mode;
+
+    ///internal
+        moTimer TimerFullRevelation; ///begins on first motion activity!!!!
+        moTimer TimerFullRestoration;///begins on full revelation finished....
+        moTimer TimerOfRevelation; ///begins on revealing all
+        moTimer TimerOfRestoration;///begins on restoring all
+
+        moTimer FeatureActivity;///start on first feature activity, ends on
+        moTimer MotionActivity;///start on first motion activity, ends on no motion
+        moTimer NoMotionActivity;///start on no motion, ends on first motion activity
+
+        enumRevelationStatus revelation_status; /// 5: full revealed 0: full hidden
+
+        long    m_Rate;
+
+        long    last_tick;
+
+
+};
+
+class moEffectParticlesSimpleFactory : public moEffectFactory
+{
+public:
+    moEffectParticlesSimpleFactory() {}
+    virtual ~moEffectParticlesSimpleFactory() {}
+    moEffect* Create();
+    void Destroy(moEffect* fx);
+};
+
+extern "C"
+{
+MO_PLG_API moEffectFactory* CreateEffectFactory();
+MO_PLG_API void DestroyEffectFactory();
+}
+
+#endif
