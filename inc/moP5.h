2df1f8bb95f65a2d30cace3074c281ab0d6d927b
fabricio_costa <fabricio_costa@c963c68c-f84c-0410-a3d6-bef16c67260e>
2010-03-01 15:37:25 +0000

    Fill mode added, ellipse implementation, still need to implement and test primitives. Must be on every moEffect as lua script functions...
    git-svn-id: https://moldeo.svn.sourceforge.net/svnroot/moldeo/trunk@265 c963c68c-f84c-0410-a3d6-bef16c67260e

--- inc/moP5.h
+++ inc/moP5.h
@@ -42,9 +42,9 @@ class LIBMOLDEO_API moP5 : public moAbstract
 {
 public:
 	moP5();
-	
+
 	/**
-     * Un triángulo es un plano creado al conectar tres puntos. Los dos primeros argumentos 
+     * Un triángulo es un plano creado al conectar tres puntos. Los dos primeros argumentos
 	 * especifican el primer punto, los dos argumentos centrales especifican el segundo punto,
 	 * y los dos últimos argumentos especifican el tercer punto.
 	 * @param x1 coordenada x del primer punto.
@@ -57,7 +57,7 @@ public:
 	void triangle(float x1, float y1, float x2, float y2, float x3, float y3);
 
 	/**
-	 * Dibuja una línea (el recorrido más directo entre dos puntos) en la pantalla. 
+	 * Dibuja una línea (el recorrido más directo entre dos puntos) en la pantalla.
 	 * Esta versión de line() con cuatro parámetros dibuja la línea en 2D.
 	 * Para colorear una línea, usar la función stroke(). Una línea no puede ser rellenada, por lo
 	 * tanto el método fill() no afecta el color de una línea. Líneas 2D son dibujadas con un ancho
@@ -154,7 +154,7 @@ public:
 
 	/**
 	 * Setea el ancho del trazo utilizado para líneas, puntos y el borde alrededor de las figuras. Todos los anchos
-	 * son especificados en unidades de píxel. 
+	 * son especificados en unidades de píxel.
 	 * @param width el ancho (en píxeles) del trazo.
 	 */
 	void strokeWeight(float width);
@@ -169,10 +169,10 @@ public:
 	//Sets the style of the joints which connect line segments. These joints are either mitered, beveled, or rounded and specified with the corresponding parameters MITER, BEVEL, and ROUND. The default joint is MITER.
 	void strokeJoin(moP5StrokeJoinMode MODE);
 
-	//The origin of the ellipse is modified by the ellipseMode() function. The default configuration is ellipseMode(CENTER), which specifies the location of the ellipse as the center of the shape. The RADIUS mode is the same, but the width and height parameters to ellipse() specify the radius of the ellipse, rather than the diameter. The CORNER mode draws the shape from the upper-left corner of its bounding box. The CORNERS mode uses the four parameters to ellipse() to set two opposing corners of the ellipse's bounding box. 
+	//The origin of the ellipse is modified by the ellipseMode() function. The default configuration is ellipseMode(CENTER), which specifies the location of the ellipse as the center of the shape. The RADIUS mode is the same, but the width and height parameters to ellipse() specify the radius of the ellipse, rather than the diameter. The CORNER mode draws the shape from the upper-left corner of its bounding box. The CORNERS mode uses the four parameters to ellipse() to set two opposing corners of the ellipse's bounding box.
 	void ellipseMode(moP5ShapeMode MODE);
 
-	//Modifies the location from which rectangles draw. The default mode is rectMode(CORNER), which specifies the location to be the upper left corner of the shape and uses the third and fourth parameters of rect() to specify the width and height. The syntax rectMode(CORNERS) uses the first and second parameters of rect() to set the location of one corner and uses the third and fourth parameters to set the opposite corner. The syntax rectMode(CENTER) draws the image from its center point and uses the third and forth parameters of rect() to specify the image's width and height. The syntax rectMode(RADIUS) draws the image from its center point and uses the third and forth parameters of rect() to specify half of the image's width and height. 
+	//Modifies the location from which rectangles draw. The default mode is rectMode(CORNER), which specifies the location to be the upper left corner of the shape and uses the third and fourth parameters of rect() to specify the width and height. The syntax rectMode(CORNERS) uses the first and second parameters of rect() to set the location of one corner and uses the third and fourth parameters to set the opposite corner. The syntax rectMode(CENTER) draws the image from its center point and uses the third and forth parameters of rect() to specify the image's width and height. The syntax rectMode(RADIUS) draws the image from its center point and uses the third and forth parameters of rect() to specify half of the image's width and height.
 	void rectMode(moP5ShapeMode MODE);
 
 	//Sets the style for rendering line endings. These ends are either squared, extended, or rounded and specified with the corresponding parameters SQUARE, PROJECT, and ROUND. The default cap is ROUND.
@@ -201,7 +201,7 @@ public:
 	 * @param range2 Rango para el componente verde o saturación dependiendo del modo seleccionado.
 	 * @param range3 Rango para el componente azul o brillo dependiendo del modo seleccionado.
 	 * @param range4 Rango para el componente alpha (transparencia).
-	 * @see fill() 
+	 * @see fill()
 	 * @see stroke()
 	 * @see background()
 	 */
@@ -211,7 +211,7 @@ public:
 	void colorMode(int mode, float range1, float range2, float range3, float range4);
 
 	/**
-	 * Sets the color used to draw lines and borders around shapes. This color is either specified in terms of the RGB or HSB color depending on the current colorMode() (the default color space is RGB, with each value in the range from 0 to 255). 
+	 * Sets the color used to draw lines and borders around shapes. This color is either specified in terms of the RGB or HSB color depending on the current colorMode() (the default color space is RGB, with each value in the range from 0 to 255).
 	 * @param gray especifica un valor entre blanco y negro.
 	 * @param alpha opacidad del trazo.
 	 * @param value1 valor de rojo o tinte (dependiendo del modo de color actual).
@@ -252,7 +252,7 @@ public:
 	void fill(float value1, float value2, float value3, float alpha);
 
 	/**
-	 * Agrega la matriz de transformaciones actual sobre la pila de matrices. Entender pushMatrix() y popMatrix() 
+	 * Agrega la matriz de transformaciones actual sobre la pila de matrices. Entender pushMatrix() y popMatrix()
 	 * requiere entender el concepto de pila de matrices. La función pushMatrix() salva el sistema coordenadas actual
 	 * en la pila y popMatrix() restaura el sistema de coordenadas anterior. pushMatrix() and popMatrix() son utilizados
 	 * en conjunto con ls otros métodos de transformación y pueden ser anidados para controlar el alcance de las transformaciones.
@@ -261,7 +261,7 @@ public:
 	void pushMatrix();
 
 	/**
-	 * Retira la matriz de transformaciones actual de la pila de matrices. Entender pushMatrix() y popMatrix() 
+	 * Retira la matriz de transformaciones actual de la pila de matrices. Entender pushMatrix() y popMatrix()
 	 * requiere entender el concepto de pila de matrices. La función pushMatrix() salva el sistema coordenadas actual
 	 * en la pila y popMatrix() restaura el sistema de coordenadas anterior. pushMatrix() and popMatrix() son utilizados
 	 * en conjunto con ls otros métodos de transformación y pueden ser anidados para controlar el alcance de las transformaciones.
@@ -278,7 +278,7 @@ public:
 	 * Aumenta y disminuy el tamaño de un objeto expandiendo y contrayendo vértices. Valores de escala son especificados
 	 * como porcentajes decimales. El llamado scale(2.0) aumenta las dimensiones de una figura en un 200%.
 	 * Los objetos siempre son escalados desde su orígen relativo al orígen de coordenadas. Las transformaciones se aplican
-	 * a todo lo que ocurre despues y las llamadas subsequentes a esta función multiplican el efecto. Por ejemplo, llamando scale(2.0) 
+	 * a todo lo que ocurre despues y las llamadas subsequentes a esta función multiplican el efecto. Por ejemplo, llamando scale(2.0)
 	 * y luego scale(1.5) es equivalente a scale(3.0). Esta función puede ser controlada adicionalmente con pushMatrix() y popMatrix().
 	 * @param size 	porcentaje a escalar el objeto.
 	 * @param x porcentaje a escalar el objeto a lo largo del eje "x".
@@ -293,9 +293,9 @@ public:
 
 	/**
 	 * Especifica una cantidad a desplazar los objetos dentro de la pantalla. El parámetro x especifica translación de izquierda
-	 * a derecha, el parámetro y especifica translación de arriba hacia abajo, y parámetro z especifica translaciones 
+	 * a derecha, el parámetro y especifica translación de arriba hacia abajo, y parámetro z especifica translaciones
 	 * hacia y desde la pantalla. Las transformaciones se aplican a todo lo que ocurre despues y llamadas subsiguientes
-	 * a esta función acumlan el efecto. Por ejemplo, llamar translate(50, 0) y luego translate(20, 0) es equivalente a 
+	 * a esta función acumlan el efecto. Por ejemplo, llamar translate(50, 0) y luego translate(20, 0) es equivalente a
 	 * translate(70, 0). Esta función puede ser controlada adicionalmente con pushMatrix() y popMatrix().
 	 * @param x translación izquierda/derecha.
 	 * @param y translación arriba/abajo.
@@ -307,10 +307,10 @@ public:
 	void translate(float x, float y, float z);
 
 	/**
-	 * Gira un objeto en la cantidad especificada por el parámero de ángulo. Los ángulos deben ser especificados 
+	 * Gira un objeto en la cantidad especificada por el parámero de ángulo. Los ángulos deben ser especificados
 	 * en radianes (valores de 0 to PI*2) o convertidos a radianes con la función radians(). Los objetos son siempre
 	 * rotados alrededor de su posición relativa al orgígen y números positivos giran los objetos en la dirección horaria.
-	 * Las transformaciones se aplican a todo lo que ocurre después y llamadas subsequentes a la función acumulan 
+	 * Las transformaciones se aplican a todo lo que ocurre después y llamadas subsequentes a la función acumulan
 	 * el efecto. Por ejemplo, llamar rotate(PI/2) y luego rotate(PI/2) es lo mismo que rotate(PI). Técnicamente, rotate()
 	 * multiplica la matriz de transformación acutal por una matriz de rotación. Esta función puede ser controlada adicionalmente
 	 * con pushMatrix() y popMatrix().
@@ -320,7 +320,7 @@ public:
 	 */
 	void rotate(float angle);
 
-	enum moP5StrokeJoinMode 
+	enum moP5StrokeJoinMode
 	{
 		MO_P5_MITER = 0,
 		MO_P5_BEVEL = 1,
@@ -329,16 +329,16 @@ public:
 
 	enum moP5ShapeMode
 	{
-		MO_P5_CENTER = 0, 
-		MO_P5_RADIUS = 1, 
-		MO_P5_CORNER = 2, 
+		MO_P5_CENTER = 0,
+		MO_P5_RADIUS = 1,
+		MO_P5_CORNER = 2,
 		MO_P5_CORNERS = 3
 	};
 
 	enum moP5StrokeCapMode
 	{
 		MO_P5_SQUARE = 0,
-		MO_P5_PROJECT = 1, 
+		MO_P5_PROJECT = 1,
 	};
 
 	enum moP5ColorMode
@@ -347,14 +347,24 @@ public:
 		MO_P5_HSB = 1
 	};
 
+	enum moP5FillMode
+	{
+		MO_P5_NOFILL = 0,
+		MO_P5_FILL = 1
+	};
+
 	static const float MO_P5_HALF_PI;
 	static const float MO_P5_TWO_PI;
 	static const float MO_P5_PI;
 protected:
+
+
+
 	int currColorMode;
 	float colorRGBCoeff[4];
 	float colorHSBCoeff[4];
 
+	moP5FillMode fillMode;
 	float tmpColor[4];
 	float strokeColor[4];
 	float fillColor[4];
